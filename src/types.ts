234c0aa63847ed8554b47a92fadb901d03a36280
MrDHFM <106590159+MrDHFM@users.noreply.github.com>
2024-07-04 07:33:11 +0530

    Update types.ts

--- src/types.ts
+++ src/types.ts
@@ -1,11 +1,6 @@
-// types.ts
-
 export interface Product {
     id: string;
     name: string;
     price: number;
     description: string;
   }
-  
-  // Add more types/interfaces as needed for your project
-  
